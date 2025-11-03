8fa23eb3f872d0c5c32a8b67926fe92a71baac18
lsrankscom <lsranks.com@gmail.com>
2025-11-04 00:11:49 +1030

    Delete pages/nations.js

--- pages/nations.js
+++
@@ -1,9 +0,0 @@
-import Navbar from '../components/Navbar';
-export default function Page() {
-  return (
-    <main style={{ padding: '2rem', textAlign: 'center' }}>
-      <Navbar />
-      <h1>Coming soon…</h1>
-    </main>
-  );
-}
