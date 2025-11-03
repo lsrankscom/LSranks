8b7da47f7b50fce0dc65b568bfce3af0b321bc24
lsrankscom <lsranks.com@gmail.com>
2025-11-04 00:13:39 +1030

    Create admin.js

---
+++ pages/admin.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+export default function Admin() {
+  return (
+    <Layout>
+      <h1 className="text-2xl font-bold">Admin</h1>
+      <p className="text-gray-600 mt-2">Login/Backend folgt.</p>
+    </Layout>
+  );
+}
