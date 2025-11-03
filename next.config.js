b55e98103c6f1ddd4b04537a375dfed071371f99
lsrankscom <lsranks.com@gmail.com>
2025-11-04 00:38:56 +1030

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
-    locales: ['de', 'en', 'it', 'es', 'fr', 'nl'],
-    defaultLocale: 'en'
-  }
+    locales: ["en", "de", "es", "fr", "it", "nl"],
+    defaultLocale: "en",
+  },
+  // optional: falls du static export nutzt, kann das aktiv bleiben
+  // output: 'export',
 };
-export default nextConfig;
+
+module.exports = nextConfig;
