87e13133eed01c64013c24830c79e187513c5a00
lsrankscom <lsranks.com@gmail.com>
2025-11-03 23:33:34 +1030

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,12 @@
 import '../styles/globals.css';
 import { I18nProvider } from '../lib/i18n';
 import LanguageSwitcher from '../components/LanguageSwitcher';
+import NavBar from '../components/NavBar';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <I18nProvider>
-      {/* Sprachwahl global */}
+      <NavBar />
       <LanguageSwitcher />
       <Component {...pageProps} />
     </I18nProvider>
