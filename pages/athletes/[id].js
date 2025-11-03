8b2a0809d392fb2364ac5a580a41c1d8ae7c829e
lsrankscom <lsranks.com@gmail.com>
2025-11-04 00:10:40 +1030

    Create [id].js

---
+++ pages/athletes/[id].js
@@ -0,0 +1,13 @@
+// pages/athletes/[id].js
+import Layout from '../../components/Layout';
+
+export default function AthletePage() {
+  return (
+    <Layout>
+      <h1 className="text-2xl font-bold">Athlete</h1>
+      <p className="text-gray-600 mt-2">
+        Detailansicht folgt – hier kannst du später Profil, Bestzeiten, Rekorde, Starts usw. zeigen.
+      </p>
+    </Layout>
+  );
+}
