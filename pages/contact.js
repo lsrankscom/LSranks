f53dcf529e4078b55bde27aa3e8237ef82f001cf
lsrankscom <lsranks.com@gmail.com>
2025-11-03 14:06:01 +1030

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import Navbar from '../components/Navbar';
+export default function Page() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <Navbar />
+      <h1>Coming soon…</h1>
+    </main>
+  );
+}
