5b9b673ce8fa3693d48e66403e1e6d98293c5514
lsrankscom <lsranks.com@gmail.com>
2025-11-03 15:24:09 +1030

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          50:  "#eef9ff",
-          100: "#d8f0ff",
-          500: "#0ea5e9",   // primär (hellblau)
-          700: "#0369a1"    // dunkelblau
-        }
-      }
-    }
+    extend: {},
   },
   plugins: [],
 };
