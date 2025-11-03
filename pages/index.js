19c71888aefca811f0ae1a918e0f8c68e3653a36
lsrankscom <lsranks.com@gmail.com>
2025-11-03 14:02:45 +1030

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import Navbar from '../components/Navbar';
+
 export default function Home() {
   return (
     <main style={{ textAlign: 'center', padding: '4rem' }}>
+      <Navbar />
       <h1>LSRanks – Lifesaving Results & Rankings</h1>
       <p>Welcome to the global database for Pool & Ocean Lifesaving results.</p>
       <p>Willkommen bei der internationalen Plattform für Rettungssport-Ergebnisse.</p>
