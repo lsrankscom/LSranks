468abd2cdf640d76b9561df0beaeff4bfd8ed9a2
lsrankscom <lsranks.com@gmail.com>
2025-11-04 00:37:10 +1030

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        // optional: zwei Blautöne wie zuvor
-        brand: {
-          600: "#1e3a8a", // dunkler
-          400: "#3b82f6", // heller
-        },
+        brand: { 600: "#1e3a8a", 400: "#3b82f6" },
       },
     },
   },
