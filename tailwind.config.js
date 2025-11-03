6c9e97b7441e242ab751a28c8c736795f93a3057
lsrankscom <lsranks.com@gmail.com>
2025-11-03 14:57:00 +1030

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50:  "#eef9ff",
+          100: "#d8f0ff",
+          500: "#0ea5e9",   // primär (hellblau)
+          700: "#0369a1"    // dunkelblau
+        }
+      }
+    }
+  },
+  plugins: [],
+};
