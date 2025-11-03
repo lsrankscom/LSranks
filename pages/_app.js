5c0c05820ba92f3c980d4f7500048f17bfe959f8
lsrankscom <lsranks.com@gmail.com>
2025-11-04 00:36:01 +1030

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 // pages/_app.js
 import '../styles/globals.css';
+import { I18nProvider } from '../lib/i18n';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <I18nProvider initialLocale={pageProps?.locale}>
+      <Component {...pageProps} />
+    </I18nProvider>
+  );
 }
