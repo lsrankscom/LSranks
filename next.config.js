7b39ea6806052f5b005a4068823578e73f36a8f1
lsrankscom <lsranks.com@gmail.com>
2025-11-03 13:01:04 +1030

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n: {
+    locales: ['de', 'en', 'it', 'es', 'fr', 'nl'],
+    defaultLocale: 'en'
+  }
+};
+export default nextConfig;
