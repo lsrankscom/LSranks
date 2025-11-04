2fc9b10bd6d1ae2a1562bab36b6c722619755a57
lsrankscom <lsranks.com@gmail.com>
2025-11-04 13:46:05 +1030

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,26 @@
-import '../styles/globals.css';
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import "../styles/globals.css";
+import {NextIntlProvider} from "next-intl";
+import {getMessages, defaultLocale} from "../lib/i18n";
+import Navbar from "../shared/Navbar";
+
+export default function App({Component, pageProps, router}) {
+  const {messages, locale} = pageProps;
+  return (
+    <NextIntlProvider messages={messages} locale={locale || defaultLocale}>
+      <div className="navbar"><Navbar/></div>
+      <Component {...pageProps} />
+      <div className="footer container">© {new Date().getFullYear()} LifesavingRankings</div>
+    </NextIntlProvider>
+  );
 }
+
+App.getInitialProps = async ({Component, ctx}) => {
+  const locale = ctx?.query?.lang || defaultLocale;
+  const messages = await getMessages(locale);
+  let pageProps = { locale, messages };
+  if (Component.getInitialProps) {
+    const extra = await Component.getInitialProps(ctx);
+    pageProps = {...pageProps, ...extra};
+  }
+  return {pageProps};
+};
