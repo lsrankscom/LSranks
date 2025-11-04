454b0246964b2204c60f7df2519ece8a461df150
lsrankscom <lsranks.com@gmail.com>
2025-11-04 13:51:48 +1030

    Create list.js

---
+++ pages/api/competitions/list.js
@@ -0,0 +1,7 @@
+export default function handler(req,res){
+  // TODO: DB. Bis dahin Stub:
+  res.json([
+    {id:"nz2025", name:"Aon New Zealand Pool Rescue Championships 2025", city:"Auckland", pool:"LCM", date:"2025-09-26"},
+    {id:"elc2025", name:"European Lifesaving Championships 2025", city:"Szczecin", pool:"LCM", date:"2025-08-25"}
+  ]);
+}
