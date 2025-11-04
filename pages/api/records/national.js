db9eb2ffe3cbaadf2bbac5ac62dd03bdd406ecb0
lsrankscom <lsranks.com@gmail.com>
2025-11-04 13:54:32 +1030

    Create national.js

---
+++ pages/api/records/national.js
@@ -0,0 +1,5 @@
+export default async function handler(req,res){
+  // Platzhalter – später je Verband spezifizieren (DEU, NED, FRA, AUS, NZL, …)
+  // Du kannst pro Nation eigene Fetcher bauen oder Daten von Admin gepflegt speichern.
+  res.json([]);
+}
